e1ded99c0dc0cb2ad1c52dde0b1fccbe9cea8cad
unknown <wendyli8620lw@gmail.com>
2022-09-11 12:17:20 +0200

    added navigation component, fix redirect, added forms page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsPageComponent } from './forms-page/forms-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   { path: 'main', component: MainPageComponent },
+  { path: 'forms', component: FormsPageComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { FormsPageComponent } from './forms-page/forms-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    FormsPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
